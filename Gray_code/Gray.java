ec867b94208772327d456ef917d9091e86ee7385
jicbo <bojicmarko06@gmail.com>
2026-03-23 22:43:00 +0100

    Refactor binary string conversion to a separate method

--- Gray_code/Gray.java
+++ Gray_code/Gray.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 class Gray {
+    static String bin(int val, int len) {
+        return Integer.toBinaryString((1 << len) | val).substring(1);
+    }
+
     static void gen(int n) {
         int max = (1 << n) - 1;
         int base = (0 << n);
         int res = (0 << n);
+        System.out.println(bin(res, n));
         while(base<max) {
             int idx = Integer.numberOfTrailingZeros(~base);
             res ^= (1 << idx);
-            String bi = Integer.toBinaryString((1 << n) | res).substring(1); 
-            System.out.println(bi);
+            System.out.println(bin(res, n));
             base++;
         }
     }
